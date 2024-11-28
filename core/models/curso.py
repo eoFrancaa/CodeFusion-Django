ad649eed4f10c1abc4dc357120bbbbe0401769f1
LeandroVish <leandrogustavosantos.info@gmail.com>
2024-11-28 16:21:27 -0300

    course.py model correction

--- core/models/curso.py
+++ core/models/curso.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+from .disciplina import Disciplina
+
 
 class Curso(models.Model):
     nome = models.CharField(max_length=100)
+    disciplina = models.ManyToManyField(Disciplina, blank=True)
 
     def __str__(self):
         return f"{self.id} - {self.nome}"
--- core/serializers/curso.py
+++ core/serializers/curso.py
@@ -1,9 +1,19 @@
+from attrs import field
+from dill.tests.test_recursive import Model
+from pyexpat import model
 from rest_framework.serializers import ModelSerializer
 
 from core.models import Curso
+from core.models.disciplina import Disciplina
 
 
 class CursoSerializer(ModelSerializer):
     class Meta:
         model = Curso
         fields = "__all__"
+
+
+class DisciplinaCurso(ModelSerializer):
+    class Meta:
+        model = Disciplina
+        fields = "nome"
